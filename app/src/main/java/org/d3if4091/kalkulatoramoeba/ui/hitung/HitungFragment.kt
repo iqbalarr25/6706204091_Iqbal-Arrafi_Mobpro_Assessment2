5688f696e2e85ef05f2dab7647dc87ffb02e3ead
Iqbal_Arrafi <iqbalarrafi39@gmail.com>
2022-05-19 19:31:05 +0700

    Menambahkan up button

--- app/src/main/java/org/d3if4091/kalkulatoramoeba/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4091/kalkulatoramoeba/ui/hitung/HitungFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import org.d3if4091.kalkulatoramoeba.R
 import org.d3if4091.kalkulatoramoeba.databinding.FragmentHitungBinding
